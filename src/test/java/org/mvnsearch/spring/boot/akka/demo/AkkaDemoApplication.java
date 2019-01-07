610f39620da6974d233cbbb3305c0baf79df8f0c
linux_china <libing.chen@gmail.com>
2019-01-07 15:51:21 -0800

    Akka demo application

---
+++ src/test/java/org/mvnsearch/spring/boot/akka/demo/AkkaDemoApplication.java
@@ -0,0 +1,18 @@
+package org.mvnsearch.spring.boot.akka.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Akka demo application
+ *
+ * @author linux_china
+ */
+@SpringBootApplication
+public class AkkaDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AkkaDemoApplication.class, args);
+    }
+
+}
