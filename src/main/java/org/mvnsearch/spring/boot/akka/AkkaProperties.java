3f5c76c2c09c67d2caa4e0ddc0a98475b0052ad0
linux_china <libing.chen@gmail.com>
2019-01-08 16:44:36 -0800

    add configuration

--- src/main/java/org/mvnsearch/spring/boot/akka/AkkaProperties.java
+++ src/main/java/org/mvnsearch/spring/boot/akka/AkkaProperties.java
@@ -3,6 +3,8 @@ package org.mvnsearch.spring.boot.akka;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.core.io.Resource;
 
+import java.util.List;
+
 /**
  * Akka properties
  *
@@ -16,7 +18,22 @@ public class AkkaProperties {
      * akka configuration, such as classpath:/akka-actor-system-1.conf
      */
     private Resource conf;
-
+    /**
+     * actor provider: local, remote, cluster
+     */
+    private String actorProvider = "local";
+    /**
+     * akka remote local listen port
+     */
+    private Integer listenPort = -1;
+    /**
+     * cluster seed-nodes, such as akka.tcp://ClusterSystem@127.0.0.1:2551, akka.tcp://ClusterSystem@127.0.0.1:2552
+     */
+    private List<String> clusterSeedNodes;
+    /**
+     * akka extensions
+     */
+    private List<String> extensions;
 
     public Resource getConf() {
         return conf;
@@ -24,5 +41,37 @@ public class AkkaProperties {
 
     public void setConf(Resource conf) {
         this.conf = conf;
+    }
+
+    public String getActorProvider() {
+        return actorProvider;
+    }
+
+    public void setActorProvider(String actorProvider) {
+        this.actorProvider = actorProvider;
+    }
+
+    public Integer getListenPort() {
+        return listenPort;
+    }
+
+    public void setListenPort(Integer listenPort) {
+        this.listenPort = listenPort;
+    }
+
+    public List<String> getClusterSeedNodes() {
+        return clusterSeedNodes;
+    }
+
+    public void setClusterSeedNodes(List<String> clusterSeedNodes) {
+        this.clusterSeedNodes = clusterSeedNodes;
+    }
+
+    public List<String> getExtensions() {
+        return extensions;
+    }
+
+    public void setExtensions(List<String> extensions) {
+        this.extensions = extensions;
     }
 }
