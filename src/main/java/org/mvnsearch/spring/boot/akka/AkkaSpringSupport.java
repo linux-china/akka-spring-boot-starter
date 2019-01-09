1c5a2cb546e249d97ea52c03202b88a3b5268b02
linux_china <libing.chen@gmail.com>
2019-01-08 17:07:19 -0800

    add actorSelection

--- src/main/java/org/mvnsearch/spring/boot/akka/AkkaSpringSupport.java
+++ src/main/java/org/mvnsearch/spring/boot/akka/AkkaSpringSupport.java
@@ -2,6 +2,7 @@ package org.mvnsearch.spring.boot.akka;
 
 import akka.actor.Actor;
 import akka.actor.ActorRef;
+import akka.actor.ActorSelection;
 import akka.actor.ActorSystem;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -16,5 +17,9 @@ public class AkkaSpringSupport {
 
     protected ActorRef actorOf(Class<? extends Actor> actorClass) {
         return actorSystem.actorOf(SpringAkkaExtension.SPRING_EXTENSION_PROVIDER.get(actorSystem).props(actorClass), actorClass.getCanonicalName());
+    }
+
+    protected ActorSelection actorSelection(String path) {
+        return actorSystem.actorSelection(path);
     }
 }
